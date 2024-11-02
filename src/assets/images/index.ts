e5bd4ab9cb22033d30566a299a0f2103b8d2e533
felikeys <felixnkrumah567@gmail.com>
2024-11-02 19:39:39 +0000

    create a page and auth folder insert image and create login file

---
+++ src/assets/images/index.ts
@@ -0,0 +1,2 @@
+export { default as LoginImage } from "./login-image.webp";
+export { default as ResetPasswordImage } from "./reset-pasword-image.avif";
--- src/routes/auth-layout.tsx
+++ src/routes/auth-layout.tsx
@@ -1,7 +1,15 @@
+import { LoginImage } from "@/assets/images";
 
 const AuthLayout = () => {
   return (
-    <div>auth-layout</div>
+    <div className="grid grid-cols-3 w-full h-full">
+    <div className="col-span-1">
+        <img className="h-full w-full object-cover" src={LoginImage} alt="login and signup image" />
+    </div>
+    <div className="col-span-2 text-2xl flex justify-center items-center">
+    <div>Profile</div>
+    </div>
+    </div>
   )
 }
 
